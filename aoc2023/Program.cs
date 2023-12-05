e355c81ea896a0e34d48ce5d214804f40dc9a576
Szczepan Ćwikliński <sec@thinkcode.pl>
2023-12-05 09:57:11 +0100

    Run logic change

--- aoc2023/Program.cs
+++ aoc2023/Program.cs
@@ -10,10 +10,13 @@ global using aoc2023.Code;
 global using System.Collections;
 global using System.Numerics;
 
+var d = DateTime.Now.Day - 1;
 if (args.Length == 1)
 {
-    var days = new BaseDay[]
-    {
+    d = int.Parse(args[0]) - 1;
+}
+var days = new BaseDay[]
+{
         new Day01(), new Day02(), new Day03(), new Day04(),
         new Day05(), new Day06(), new Day07(), new Day08(),
         new Day09(), new Day10(), new Day11(), new Day12(),
@@ -21,24 +24,7 @@ if (args.Length == 1)
         new Day17(), new Day18(), new Day19(), new Day20(),
         new Day21(), new Day22(), new Day23(), new Day24(),
         new Day25()
-    };
-    var d = int.Parse(args[0]) - 1;
+};
 
-    days[d].Solve(true);
-    days[d].Solve(false);
-}
-else
-{
-    var day = DateTime.Now.Day;
-
-    var t = Assembly.GetExecutingAssembly()
-        .GetTypes()
-        .Where(x => x.Name == $"Day{day:d2}")
-        .Single();
-
-    if (Activator.CreateInstance(t) is BaseDay c)
-    {
-        c.Solve(true);
-        c.Solve(false);
-    }
-}
+days[d].Solve(true);
+days[d].Solve(false);
